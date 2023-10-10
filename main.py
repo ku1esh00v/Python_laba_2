137a617f477b9d8884a7d3d4f5be65655492b8b7
ku1esh00v <oleg.kuleshov.1261@mail.ru>
2023-10-10 17:39:06 +0300

    Оптимизировал часть условного оператора

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ m = int(input())
 n = int(input())
 if m <= n:
     for i in range(m, n + 1):
-        if i == 9 or (i + 1) % 10 == 0 or i % 17 == 0 or (i % 3 == 0 and i % 5 == 0):
+        if (i % 10 == 9) or (i % 17 == 0) or (i % 3 == 0 and i % 5 == 0):
             print(i)
